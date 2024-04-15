03d7fca9b6bc90d96b1b1415e99d4712a10b279a
M.Jrad <mjrad@kamioun.tn>
2024-04-15 11:21:47 +0100

    :recycle: refactor(uncommit TopNavBar elements):

--- src/components/blocks/TopNavBar/TopNavBar.tsx
+++ src/components/blocks/TopNavBar/TopNavBar.tsx
@@ -1,7 +1,7 @@
-// import LanguageSwitcher from "@/elements/LanguageSwitcher";
-// import ModeSwitcher from "@/elements/ModeSwitcher";
-// import Notification from "@/elements/Notification";
-// import Profile from "@/elements/Profile";
+import LanguageSwitcher from "@/elements/LanguageSwitcher";
+import ModeSwitcher from "@/elements/ModeSwitcher";
+import Notification from "@/elements/Notification";
+import Profile from "@/elements/Profile";
 
 const TopNavBar = () => {
   return (
@@ -11,10 +11,10 @@ const TopNavBar = () => {
 md:py-4 xxl:py-6 gap-3 bg-n0 dark:bg-bg4 fixed flex justify-end items-center h-[90px]"
     >
       <div className="flex items-center gap-3 sm:gap-4 xxl:gap-6">
-        {/* <ModeSwitcher />
+        <ModeSwitcher />
         <Notification />
         <LanguageSwitcher />
-        <Profile /> */}
+        <Profile />
       </div>
     </nav>
   );
