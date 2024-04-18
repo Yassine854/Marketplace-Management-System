fc07cd82cf87152aaa418de6bb00129eddbfc381
M.Jrad <mjrad@kamioun.tn>
2024-04-18 10:27:06 +0100

    :sparkles: feat(OrdersTable): typesense demo

--- src/app/[locale]/dashboard/orders/open/page.tsx
+++ src/app/[locale]/dashboard/orders/open/page.tsx
@@ -1,5 +1,30 @@
-import OpenOrdersTemplate from "@/components/templates/OpenOrdersTemplate";
+"use client";
 
-const OrdersPage = () => <OpenOrdersTemplate />;
+import OpenOrdersTemplate from "@/components/templates/OpenOrdersTemplate";
+import { typesenseClient } from "@/libs/typesenseClient";
+import { useEffect } from "react";
+
+const searchParameters = {
+  q: "ali",
+  query_by: "customer_firstname",
+  sort_by: "customer_id:desc",
+};
+
+const OrdersPage = () => {
+  useEffect(() => {
+    typesenseClient
+      .collections("orders")
+      .documents()
+      .search(searchParameters)
+      .then((res) => {
+        console.log("🚀 ~ .then ~ res:", res);
+      })
+      .catch((err) => {
+        console.log("🚀 ~ .then ~ err:", err);
+      });
+  }, []);
+
+  return <OpenOrdersTemplate />;
+};
 
 export default OrdersPage;
