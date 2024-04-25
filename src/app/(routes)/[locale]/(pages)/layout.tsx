f118e3d81f97ab18ebb034c0daab5c8f74959cfa
M.Jrad <mjrad@kamioun.tn>
2024-04-25 12:58:24 +0100

    :lipstick: style(layout): make the subPages layout scrollable

--- src/app/(routes)/[locale]/(pages)/layout.tsx
+++ src/app/(routes)/[locale]/(pages)/layout.tsx
@@ -11,7 +11,7 @@ const PagesLayout = ({ children }: Props) => {
     <div className="h-screen w-screen ">
       <TopNavBar />
       <Sidebar />
-      <div className="fixed bottom-0 left-64 right-0  top-16 z-10  rounded-xl bg-n30 p-3">
+      <div className="fixed bottom-0 left-64 right-0  top-16 z-10  overflow-x-scroll rounded-xl bg-n30 p-3">
         {children}
       </div>
     </div>
