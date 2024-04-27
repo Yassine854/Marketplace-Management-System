230c9d3fa639b383d1d4f0d8b853522cdcdc51b6
M.Jrad <mjrad@kamioun.tn>
2024-04-27 11:50:40 +0100

    :truck: chore(Storybook): next wrapper

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,8 +2,16 @@ import "../public/styles/globals.css";
 
 import type { Preview } from "@storybook/react";
 
+//import { RouterContext } from "next/dist/shared/lib/router-context.shared-runtime";
+
 const preview: Preview = {
   parameters: {
+    // nextRouter: {
+    //   Provider: RouterContext.Provider,
+    // },
+    // nextjs: {
+    //   appDirectory: true,
+    // },
     controls: {
       matchers: {
         color: /(background|color)$/i,
