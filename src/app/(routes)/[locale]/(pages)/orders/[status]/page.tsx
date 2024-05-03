fa7adabe124c6469d1fe691423803b7d900f645b
M.Jrad <mjrad@kamioun.tn>
2024-05-03 11:31:31 +0100

    :bug: fix(Storybook): fix storybook

--- src/app/(routes)/[locale]/(pages)/orders/[status]/page.tsx
+++ src/app/(routes)/[locale]/(pages)/orders/[status]/page.tsx
@@ -6,60 +6,14 @@ import AdvancedSearch from "@/components/blocks/AdvancedSearch";
 import Box from "@/components/blocks/Box";
 import OrdersTable from "@/components/blocks/OrdersTable";
 import OrdersTable2 from "@/components/blocks/OrdersTable2";
+import OrdersTemplate from "@/components/templates/OrdersTemplate";
 import { useNavigation } from "@/hooks/useNavigation";
 
 const OrdersPage = () => {
   const [isAdvanced, setAdvanced] = useState(true);
   const { status } = useNavigation();
 
-  return (
-    <>
-      {(status === "open" || status === "valid" || status === "ready") && (
-        <OrdersTable status={status} />
-      )}
-      {status !== "open" && isAdvanced && (
-        <Box>
-          <div className="flex h-full items-center justify-center">
-            <AdvancedSearch
-              onSearch={() => setAdvanced(false)}
-              status={status}
-            />
-          </div>
-        </Box>
-      )}
-      {status !== "valid" && isAdvanced && (
-        <Box>
-          <div className="flex h-full items-center justify-center">
-            <AdvancedSearch
-              onSearch={() => setAdvanced(false)}
-              status={status}
-            />
-          </div>
-        </Box>
-      )}
-      {status !== "ready" && isAdvanced && (
-        <Box>
-          <div className="flex h-full items-center justify-center">
-            <AdvancedSearch
-              onSearch={() => setAdvanced(false)}
-              status={status}
-            />
-          </div>
-        </Box>
-      )}
-
-      {status !== "open" &&
-        status !== "valid" &&
-        status !== "ready" &&
-        !isAdvanced && (
-          <OrdersTable2
-            isAdvanced={isAdvanced}
-            setAdvanced={setAdvanced}
-            status={status}
-          />
-        )}
-    </>
-  );
+  return <OrdersTemplate />;
 };
 
 export default OrdersPage;
