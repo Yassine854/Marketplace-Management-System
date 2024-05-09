85d971b6d534d2e332b69b492bcc325eb48583ea
M.Jrad <mjrad@kamioun.tn>
2024-05-09 16:23:54 +0100

    :bug: fix(Auth): change env variable NEXTAUTH_SECRET

--- src/libs/next-auth/auth.ts
+++ src/libs/next-auth/auth.ts
@@ -20,7 +20,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     }),
   ],
 
-  secret: process.env.SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   debug: process.env.NODE_ENV === "development",
   callbacks: {
     async jwt({ token, user }) {
