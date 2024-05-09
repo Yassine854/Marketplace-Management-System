c8ab77c444821ace55eb610aeb772e2a63b0f6fc
M.Jrad <mjrad@kamioun.tn>
2024-05-09 17:54:10 +0100

    :bug: fix(Auth): add secret to auth.ts

--- src/libs/next-auth/auth.ts
+++ src/libs/next-auth/auth.ts
@@ -20,6 +20,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     }),
   ],
   trustHost: true,
+  secret: process.env.AUTH_SECRET,
   callbacks: {
     async jwt({ token, user }) {
       //@ts-ignore
