040c775036574b31ce2c9375a268fd11cde81540
M.Jrad <m.jrad@kamioun.tn>
2024-05-14 19:06:29 +0100

    :sparkles: feat(Api): trying the new typesense schema

--- src/app/api/graphql/resolvers/getOrders.ts
+++ src/app/api/graphql/resolvers/getOrders.ts
@@ -9,20 +9,34 @@ export type GetOrdersParams = {
   search: string;
 };
 
+const unixTimestampToDate = (timestamp: number) => {
+  // Create a new Date object using the provided timestamp
+  const dateObject = new Date(timestamp);
+
+  // Extract day, month, and year components from the Date object
+  const day = String(dateObject.getDate()).padStart(2, "0");
+  const month = String(dateObject.getMonth() + 1).padStart(2, "0"); // Adding 1 because months are 0-indexed
+  const year = dateObject.getFullYear();
+
+  // Return the date string in the format "DD/MM/YYYY"
+  return `${day}/${month}/${year}`;
+};
+
 const createOrdersList = (typesenseHits: any): Order[] =>
-  typesenseHits.map(
-    ({ document }: any): Order => ({
-      id: document.extension_attributes.kamioun_order_id,
+  typesenseHits.map(({ document }: any): Order => {
+    console.log("🚀 ~ returntypesenseHits.map ~ document:", document.items);
+    return {
+      id: document.id,
       customer: {
         id: document.customer_id,
         name: document.customer_firstname + " " + document.customer_lastname,
       },
-      total: document.subtotal,
-      deliveryDate: document.extension_attributes.delivery_date,
+      total: document.total,
+      deliveryDate: unixTimestampToDate(document.delivery_date),
       isSelected: false,
       lines: [],
-    }),
-  );
+    };
+  });
 
 export const getOrders = async ({
   status,
@@ -35,27 +49,34 @@ export const getOrders = async ({
 > => {
   try {
     let searchParams;
+
     const searchParamsWithoutFilter = {
       q: search,
-      query_by: "customer_firstname",
+      query_by: "customer_firstname,customer_lastname",
       page: page,
       per_page: perPage,
       sort_by: sortBy,
+      filter_by: "created_at:[1702531200000..1735104000000]",
     };
 
     if (status) {
       const filter = {
-        filter_by: "status:= " + status,
+        filter_by:
+          "status:= " + status + " && created_at:[16531200000..1675104000000]",
       };
-      searchParams = Object.assign(searchParamsWithoutFilter, filter);
+
+      searchParams = Object.assign(
+        searchParamsWithoutFilter,
+        "created_at:[1672531200000..1675104000000]",
+      );
     } else {
       searchParams = searchParamsWithoutFilter;
     }
 
     const typesenseResponse = await typesenseClient
-      .collections("orders")
+      .collections("orders2")
       .documents()
-      .search(searchParams);
+      .search(searchParamsWithoutFilter);
 
     const orders: Order[] = createOrdersList(typesenseResponse?.hits);
 
---
+++ src/app/api/graphql/resolvers/unixTimestampToDate.ts
@@ -0,0 +1,12 @@
+const unixTimestampToDate = (timestamp = {
+  // Create a new Date object using the provided timestamp
+  const: (dateObject = new Date(timestamp)),
+
+  // Extract day, month, and year components from the Date object
+  const: (day = String(dateObject.getDate()).padStart(2, "0")),
+  const: (month = String(dateObject.getMonth() + 1).padStart(2, "0")), // Adding 1 because months are 0-indexed
+  const: (year = dateObject.getFullYear()),
+
+  // Return the date string in the format "DD/MM/YYYY"
+  return: `${day}/${month}/${year}`,
+});
