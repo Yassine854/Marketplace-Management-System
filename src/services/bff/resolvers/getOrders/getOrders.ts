066cee657e3cee56c01ad9e9cc7640a102c8e765
M.Jrad <m.jrad@kamioun.tn>
2024-05-16 15:06:03 +0100

    :sparkles: feat(API): getOrders

--- src/services/bff/resolvers/getOrders/getOrders.ts
+++ src/services/bff/resolvers/getOrders/getOrders.ts
@@ -1,20 +1,9 @@
 import { GetOrdersParams } from "./getOrders.types";
 import { Order } from "@/types/order";
 import { typesenseClient } from "@/libs/typesenseClient";
-import { unixTimestampToDate } from "@/utils/unixTimestampToDate";
 
 const createOrdersList = (typesenseHits: any): Order[] =>
-  typesenseHits.map(({ document }: any): Order => {
-    return {
-      id: document.id,
-      customer: {
-        id: document.customer_id,
-        name: document.customer_firstname + " " + document.customer_lastname,
-      },
-      total: document.total,
-      deliveryDate: unixTimestampToDate(document.delivery_date),
-    };
-  });
+  typesenseHits.map(({ document }: any): Order => document);
 
 export const getOrders = async ({
   sortBy,
@@ -28,37 +17,12 @@ export const getOrders = async ({
   try {
     const searchParams = {
       q: search,
-      query_by: "customer_firstname,customer_lastname",
+      query_by: "customerFirstname,customerLastname,kamiounId",
       page: page,
       per_page: perPage,
       sort_by: sortBy,
       filter_by: filterBy,
     };
-
-    // let searchParams;
-
-    // const searchParamsWithoutFilter = {
-    //   q: search,
-    //   query_by: "customer_firstname,customer_lastname",
-    //   page: page,
-    //   per_page: perPage,
-    //   sort_by: sortBy,
-    //   filter_by: "created_at:[1702531200000..1735104000000]",
-    // };
-
-    // if (status) {
-    //   const filter = {
-    //     filter_by:
-    //       "status:= " + status + " && created_at:[16531200000..1675104000000]",
-    //   };
-
-    //   searchParams = Object.assign(
-    //     searchParamsWithoutFilter,
-    //     "created_at:[1672531200000..1675104000000]",
-    //   );
-    // } else {
-    //   searchParams = searchParamsWithoutFilter;
-    // }
 
     const typesenseResponse = await typesenseClient
       .collections("orders")
