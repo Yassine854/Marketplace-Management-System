c977c835e3f5823daa449afb90589db385ac3459
M.Jrad <m.jrad@kamioun.tn>
2024-05-16 15:29:10 +0100

    :sparkles: feat(API): getOrder

--- src/services/bff/resolvers/getOrder.ts
+++ src/services/bff/resolvers/getOrder.ts
@@ -1,42 +1,14 @@
 import { Order } from "@/types/order";
-import { OrderLine } from "@/types/OrderLine";
 import { typesenseClient } from "@/libs/typesenseClient";
 
-const formatOrder = (typesenseResponse: any): Order => {
-  const document = typesenseResponse.hits[0].document;
-
-  const lines: OrderLine[] = document.items.map((e: any) => ({
-    id: e.item_id,
-    sku: e.sku,
-    orderId: e.order_id,
-    productId: e.product_id,
-    productName: e.name,
-    quantity: e.qty_ordered,
-    productPrice: e.price,
-    totalPrice: e.price * e.qty_ordered,
-  }));
-
-  return {
-    id: document.extension_attributes?.kamioun_order_id,
-    total: document.subtotal,
-    deliveryDate: document.extension_attributes?.delivery_date,
-    isSelected: false,
-    lines: lines,
-    customer: {
-      id: document.customer_id,
-      name: document.customer_firstname + " " + document.customer_lastname,
-    },
-  };
-};
-
 export const getOrder = async (
-  id: string,
+  orderId: string,
 ): Promise<{ id: string } | undefined> => {
   try {
     const searchParams = {
-      filter_by: "customer_firstname:=brahmi",
-      q: "brahmi",
-      query_by: "customer_firstname",
+      filter_by: "id:=" + orderId,
+      q: "",
+      query_by: "kamiounId",
     };
 
     const typesenseResponse = await typesenseClient
@@ -44,7 +16,8 @@ export const getOrder = async (
       .documents()
       .search(searchParams);
 
-    const order: Order = formatOrder(typesenseResponse);
+    const hits = typesenseResponse?.hits || [];
+    const order: Order = hits[0]?.document as Order;
 
     return order;
   } catch (error) {
--- src/services/bff/resolvers/index.ts
+++ src/services/bff/resolvers/index.ts
@@ -5,6 +5,7 @@ import { getOrder } from "./getOrder";
 export const resolvers = {
   Query: {
     getOrders: (parent: any, args: GetOrdersParams) => getOrders(args),
-    getOrder: (parent: any, args: { id: string }) => getOrder(args.id),
+    getOrder: (parent: any, args: { orderId: string }) =>
+      getOrder(args.orderId),
   },
 };
