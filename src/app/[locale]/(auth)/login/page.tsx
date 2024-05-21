a34cab124ccfa55b3c3577fca42a3263171d2393
M.Jrad <m.jrad@kamioun.tn>
2024-05-21 19:13:33 +0100

    :bug: fix(Auth): add "use cleint" to the LoginPage

--- src/app/[locale]/(auth)/login/page.tsx
+++ src/app/[locale]/(auth)/login/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import LoginPage from "@/components/pages/LoginPage";
 
 const Login = () => <LoginPage />;
--- src/components/widgets/LoginForm/LoginForm.tsx
+++ src/components/widgets/LoginForm/LoginForm.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import * as z from "zod";
 
 import { IconEye, IconEyeOff } from "@tabler/icons-react";
