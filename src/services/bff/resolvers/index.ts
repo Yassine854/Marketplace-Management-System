e9c5f75fd459da25bee6f0747e7e6766035f12c8
M.Jrad <m.jrad@kamioun.tn>
2024-05-22 14:14:38 +0100

    :sparkles: feat(BFF): add getUser resolver

--- src/services/bff/resolvers/index.ts
+++ src/services/bff/resolvers/index.ts
@@ -1,11 +1,14 @@
 import { GetOrdersParams, getOrders } from "./getOrders";
 
 import { getOrder } from "./getOrder";
+import { getUser } from "./getUser";
 
 export const resolvers = {
   Query: {
     getOrders: (parent: any, args: GetOrdersParams) => getOrders(args),
     getOrder: (parent: any, args: { orderId: string }) =>
       getOrder(args.orderId),
+    getUser: (parent: any, args: { username: string }) =>
+      getUser(args.username),
   },
 };
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,8 +1,9 @@
 export type User = {
   id: string;
   username: string;
-  email?: string;
+  email?: string | null;
   password: string;
+  status: string;
   role: string;
   warehouses: string[];
 };
