d714954a8cabdb3eae63f5a3cc91cf372a02fd4d
M.Jrad <mjrad@kamioun.tn>
2024-05-28 11:56:40 +0100

    :recycle: refactor(OrdersTable): remove AdvancedSearch Component

--- src/components/widgets/LoginForm/formSchema.ts
+++ src/components/widgets/LoginForm/formSchema.ts
@@ -1,10 +1,6 @@
 import * as z from "zod";
 
 export const FormSchema = z.object({
-  username: z
-    .string()
-    .min(4, { message: "Username must be at least 4 characters." }),
-  password: z
-    .string()
-    .min(6, { message: "Password must be at least 6 characters." }),
+  username: z.string().min(4, { message: "Please enter your username " }),
+  password: z.string().min(6, { message: "Please enter your password " }),
 });
