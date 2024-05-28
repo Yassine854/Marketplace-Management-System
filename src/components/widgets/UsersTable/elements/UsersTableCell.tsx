39a7e8745a00f804e45bdf59e0fb7ff38d701ca3
M.Jrad <mjrad@kamioun.tn>
2024-05-28 13:45:05 +0100

    :sparkles: feat(UsersTable): row items

--- src/components/widgets/UsersTable/elements/UsersTableCell.tsx
+++ src/components/widgets/UsersTable/elements/UsersTableCell.tsx
@@ -1,5 +1,5 @@
 const OrdersTableCell = ({ children }: any) => (
-  <td className="h-20   border-2  ">
+  <td className="   border-2  ">
     <div className="flex  select-none items-center justify-center   truncate text-ellipsis p-4 text-sm">
       {children}
     </div>
--- src/components/widgets/UsersTable/elements/UsersTableRow.tsx
+++ src/components/widgets/UsersTable/elements/UsersTableRow.tsx
@@ -3,29 +3,56 @@ import UsersTableCell from "./UsersTableCell";
 
 const OrdersTableRow = ({ user }: any) => {
   console.log("🚀 ~ OrdersTableRow ~ user:", user);
-
+  const status = "New";
+  //const warehouses = ["mghira", "sousse", "shargasfasdfia", "ariana", "all"];
+  const warehouses = ["all"];
   return (
     <tr className=" even:bg-primary/5 hover:bg-n30 dark:even:bg-bg3">
       <UsersTableCell>
-        <p className=" truncate text-ellipsis text-sm">{user.id}</p>
+        <p className=" truncate text-ellipsis text-sm">{user.username}</p>
       </UsersTableCell>
       <UsersTableCell>
-        <p className=" truncate text-ellipsis text-sm">{user.id} </p>
+        <p className=" truncate text-ellipsis text-sm">{user.username} </p>
       </UsersTableCell>
       <UsersTableCell>
-        <p className=" truncate text-ellipsis text-sm">{user.id} </p>
+        <p className=" truncate text-ellipsis text-sm">{user.username} </p>
       </UsersTableCell>
       <UsersTableCell>
-        <p className="truncate ">{user.id}</p>
+        <span
+          key={1}
+          className="text-secondary m-2 block w-16 overflow-hidden text-ellipsis rounded-xl border border-n30 bg-secondary1/10 p-1 py-2 text-center text-xs font-bold dark:border-n500 dark:bg-bg3 "
+        >
+          Admin
+        </span>
       </UsersTableCell>
       <UsersTableCell>
-        <p className="truncate ">{user.id}</p>
+        <div className="flex max-w-36 flex-grow flex-wrap items-center justify-center">
+          {warehouses.map((warehouse: any) => (
+            <span
+              key={1}
+              className="text-secondary m-2 block w-16 overflow-hidden text-ellipsis rounded-xl border border-n30 bg-secondary1/10 p-1 py-2 text-center text-xs font-bold dark:border-n500 dark:bg-bg3 "
+            >
+              {warehouse}
+            </span>
+          ))}
+        </div>
       </UsersTableCell>
       <UsersTableCell>
-        <p className="truncate ">{user.id}</p>
+        <span
+          className={`block w-28 rounded-xl border border-n30 py-2 text-center text-xs font-bold dark:border-n500 xxl:w-36 ${
+            status == "New" && "bg-primary/10 text-primary dark:bg-bg3"
+          } ${
+            status == "Available" &&
+            "bg-secondary1/10 text-secondary1 dark:bg-bg3"
+          } ${
+            status == "Busy" && "bg-secondary2/10 text-secondary2 dark:bg-bg3"
+          } `}
+        >
+          {status}
+        </span>
       </UsersTableCell>
       <UsersTableCell>
-        <p className="truncate ">{user.id}</p>
+        <p className="truncate ">{user.username}</p>
       </UsersTableCell>
       <UsersTableCell>
         <IconSettings string={2} className="cursor-pointer" />
