e1a719de1ae78fbd5674c8432ff821646eb5a630
M.Jrad <mjrad@kamioun.tn>
2024-06-03 14:17:35 +0100

    :recycle: refactor(UsersTable): add comment with cell name

--- src/components/tables/UsersTable/elements/UsersTableRow.tsx
+++ src/components/tables/UsersTable/elements/UsersTableRow.tsx
@@ -2,61 +2,67 @@ import { IconSettings } from "@tabler/icons-react";
 import UsersTableCell from "./UsersTableCell";
 
 const UsersTableRow = ({ user }: any) => {
-  let status = "New";
-  //const warehouses = ["mghira", "sousse", "shargasfasdfia", "ariana", "all"];
-  const warehouses = ["all"];
+  console.log("🚀 ~ UsersTableRow ~ user:", user);
   return (
     <tr className=" even:bg-primary/5 hover:bg-n30 dark:even:bg-bg3">
+      {/* Username */}
       <UsersTableCell>
         <p className=" truncate text-ellipsis text-sm">{user.username}</p>
       </UsersTableCell>
+      {/* Name */}
       <UsersTableCell>
-        <p className=" truncate text-ellipsis text-sm">{user.username} </p>
+        <p className=" truncate text-ellipsis text-sm">{user.name} </p>
       </UsersTableCell>
+      {/* Email */}
       <UsersTableCell>
-        <p className=" truncate text-ellipsis text-sm">{user.username} </p>
+        <p className=" truncate text-ellipsis text-sm">{user.email} </p>
       </UsersTableCell>
+      {/* Role */}
       <UsersTableCell>
         <span
           key={1}
           className="text-secondary m-2 block w-16 overflow-hidden text-ellipsis rounded-xl border border-n30 bg-secondary1/10 p-1 py-2 text-center text-xs font-bold dark:border-n500 dark:bg-bg3 "
         >
-          Admin
+          {user.roleCode}
         </span>
       </UsersTableCell>
+      {/* Warehouse */}
       <UsersTableCell>
         <div className="flex max-w-36 flex-grow flex-wrap items-center justify-center">
-          {warehouses.map((warehouse: any) => (
-            <span
-              key={1}
-              className="text-secondary m-2 block w-16 overflow-hidden text-ellipsis rounded-xl border border-n30 bg-secondary1/10 p-1 py-2 text-center text-xs font-bold dark:border-n500 dark:bg-bg3 "
-            >
-              {warehouse}
-            </span>
-          ))}
+          <span
+            key={1}
+            className="text-secondary m-2 block w-16 overflow-hidden text-ellipsis rounded-xl border border-n30 bg-secondary1/10 p-1 py-2 text-center text-xs font-bold dark:border-n500 dark:bg-bg3 "
+          >
+            {user.warehouseCode}
+          </span>
         </div>
       </UsersTableCell>
+      {/* Status */}
       <UsersTableCell>
         <span
           className={`block w-28 rounded-xl border border-n30 py-2 text-center text-xs font-bold dark:border-n500 xxl:w-36 ${
-            status == "New" && "bg-primary/10 text-primary dark:bg-bg3"
+            user.status == "New" && "bg-primary/10 text-primary dark:bg-bg3"
           } ${
-            status == "Available" &&
+            user.status == "Available" &&
             "bg-secondary1/10 text-secondary1 dark:bg-bg3"
           } ${
-            status == "Busy" && "bg-secondary2/10 text-secondary2 dark:bg-bg3"
+            user.status == "Busy" &&
+            "bg-secondary2/10 text-secondary2 dark:bg-bg3"
           } `}
         >
-          {status}
+          {user.status}
         </span>
       </UsersTableCell>
+      {/* Created At */}
       <UsersTableCell>
         <p className="truncate ">{user.username}</p>
       </UsersTableCell>
+      {/* Edit */}
       <UsersTableCell>
         <IconSettings string={2} className="cursor-pointer" />
       </UsersTableCell>
     </tr>
   );
 };
+
 export default UsersTableRow;
