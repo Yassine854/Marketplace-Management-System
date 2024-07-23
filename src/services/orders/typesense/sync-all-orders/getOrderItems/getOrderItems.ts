b29f11f7901a4258585e0e97744a733e568807f8
M.Jrad <mjrad@kamioun.tn>
2024-07-23 09:53:48 +0100

    :recycle: refactor(orders service): disable getOrderItems when syncing orders

--- src/services/orders/typesense/sync-all-orders/getOrderItems/getOrderItems.ts
+++ src/services/orders/typesense/sync-all-orders/getOrderItems/getOrderItems.ts
@@ -2,17 +2,21 @@ import { OrderLine } from "@/types/orderLine";
 
 export const getOrderItems = (orderItems: any): OrderLine[] =>
   orderItems?.map((item: any) => {
-    return {
-      id: item.item_id,
-      orderId: item.order_id,
-      kamiounOrderId: "",
-      productId: item.product_id,
-      productName: item.name,
-      productPrice: item.base_price,
-      quantity: item.qty_ordered,
-      shipped: item.qty_shipped,
-      totalPrice: item.price,
-      sku: item.sku,
-      pcb: item.weight,
-    };
+    // const qtyPcbAttribute =
+    //   item?.product_option?.extension_attributes?.custom_options.find(
+    //     (attr: any) => attr.option_id === "qty_pcb",
+    //   );
+    // const quantityPCB = qtyPcbAttribute ? qtyPcbAttribute.option_value : 0;
+    // return {
+    //   id: item.item_id,
+    //   orderId: item.order_id,
+    //   productId: item.product_id,
+    //   productName: item.name,
+    //   productPrice: item.base_price,
+    //   quantity: quantityPCB * item.qty_ordered,
+    //   shipped: item.weight,
+    //   totalPrice: item.price,
+    //   sku: item.sku,
+    //   pcb: item.qty_ordered,
+    // };
   }) || [];
