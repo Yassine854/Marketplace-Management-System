9b325e5e8d7feac856bbc8941c3d97c472144ca8
Haythem532002 <khiarih06@gmail.com>
2024-07-24 17:18:57 +0100

    :sparkles: feat(resolve conflicts):

--- src/clients/nextRoutes/index.ts
+++ src/clients/nextRoutes/index.ts
@@ -3,9 +3,9 @@ import { getNumberOfOrdersByDayRoute } from "./orders/typesense/getNumberOfOrder
 import { getOrdersByDeliveryDateRoute } from "./orders/magento/getOrdersByDeliveryDateRoute";
 import { numberOfOrdersByMonthAnalyticsRoute } from "./analytics/numberOfOrdersByMonthAnalyticsRoute";
 import { numberOfOrdersByWeekAnalyticsRoute } from "./analytics/numberOfOrdersByWeekAnalyticsRoute";
+import { getNumberOfOrdersByMonthRoute } from "./orders/typesense/getNumberOfOrdersByMonthRoute";
 // import { getOrdersByDeliveryDateRoute } from "./orders/magento/getOrdersByDeliveryDateRoute";
 // import { getNumberOfOrdersByDayRoute } from "./orders/typesense/getNumberOfOrdersByDayRoute";
-
 export const nextRoute = {
   analytics: {
     numberOfOrders: {
