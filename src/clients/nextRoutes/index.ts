0d9255892502210db2440e9ea4fabc15ba805080
medazizktata <aziiizktata7@gmail.com>
2024-07-24 13:51:00 +0100

    add: Number of orders by quarters (analytics endpoint): fetches quarter data for a single year (date parameter)

--- src/clients/nextRoutes/index.ts
+++ src/clients/nextRoutes/index.ts
@@ -1,11 +1,17 @@
 import { numberOfOrdersByMonthAnalyticsRoute } from "./analytics/numberOfOrdersByMonthAnalyticsRoute";
+import { numberOfOrdersByQuarterAnalyticsRoute } from "./analytics/numberOfOrdersByQuarterAnalyticsRoute";
+import { numberOfOrdersLifetimeAnalyticsRoute } from "./analytics/numberOfOrdersLifetimeAnalyticsRoute";
 import { getOrdersByDeliveryDateRoute } from "./orders/magento/getOrdersByDeliveryDateRoute";
-import { getNumberOfOrdersByDayRoute } from "./orders/typesense/getNumberOfOrdersByDayRoute";
+import { getNumberOfOrdersByDayRoute } from "./orders/typesense/numberOfOrders/getNumberOfOrdersByDayRoute";
+import { getNumberOfOrdersByMonthRoute } from "./orders/typesense/numberOfOrders/getNumberOfOrdersByMonthRoute";
+import { getNumberOfOrdersByQuarterRoute } from "./orders/typesense/numberOfOrders/getNumberOfOrdersByQuarterRoute";
 
 export const nextRoute = {
   analytics: {
     numberOfOrders: {
       byMonth: numberOfOrdersByMonthAnalyticsRoute,
+      byQuarter: numberOfOrdersByQuarterAnalyticsRoute,
+      lifetime: numberOfOrdersLifetimeAnalyticsRoute,
     },
   },
 
@@ -19,6 +25,8 @@ export const nextRoute = {
     typesense: {
       numberOfOrders: {
         byDay: getNumberOfOrdersByDayRoute,
+        byMonth: getNumberOfOrdersByMonthRoute,
+        byQuarter: getNumberOfOrdersByQuarterRoute,
       },
     },
   },
