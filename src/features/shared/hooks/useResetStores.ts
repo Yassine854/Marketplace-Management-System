9c6cc41add96532961fba9917880502fd183c2f0
M.Jrad <mjrad@kamioun.tn>
2024-07-31 09:22:17 +0100

    formatting

--- src/features/shared/hooks/useResetStores.ts
+++ src/features/shared/hooks/useResetStores.ts
@@ -1,21 +1,23 @@
 import { useOrdersStore } from "@/features/orderManagement/stores/ordersStore";
+import { useMilkRunStore } from "@/features/milkRunManagement/stores/milkRunStore";
 import { useOrderActionsStore } from "@/features/orderManagement/stores/orderActionsStore";
 import { useOrderDetailsStore } from "@/features/orderManagement/stores/orderDetailsStore";
 import { useOrdersTableStore } from "@/features/orderManagement/stores/ordersTableStore";
-import { useMilkRunStore } from "@/features/milkRunManagement/stores/milkRunStore";
 
 export const useResetStores = () => {
-  const resetOrderDetailsStore = useOrderDetailsStore(
+  const resetOrdersTableStore = useOrdersTableStore(
     (state: any) => state.reset,
   );
-  const resetOrdersStore = useOrdersStore((state: any) => state.reset);
+
   const resetOrderActionsStore = useOrderActionsStore(
     (state: any) => state.reset,
   );
 
-  const resetOrdersTableStore = useOrdersTableStore(
+  const resetOrderDetailsStore = useOrderDetailsStore(
     (state: any) => state.reset,
   );
+
+  const resetOrdersStore = useOrdersStore((state: any) => state.reset);
 
   const resetMilkRunStore = useMilkRunStore((state: any) => state.reset);
 
