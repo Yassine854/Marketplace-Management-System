77ae2026d48fa0871567b1f9c816ac0791bd6dba
medazizktata <aziiizktata7@gmail.com>
2024-07-31 13:29:53 +0100

    Renamed File names (Gross Marchandise Value), fixed import statements

--- src/app/api/orders/typesense/createGmvCollection/route.ts
+++ src/app/api/orders/typesense/createGmvCollection/route.ts
@@ -1,4 +1,4 @@
-import { addgmvCollection } from "@/services/orders/typesense/gmvCollection/addgmvPrevMonthsColl";
+import { addgmvCollection } from "@/services/orders/typesense/grossMarchandiseValueCollections/addgmvPrevMonthsColl";
 import { NextRequest } from "next/server";
 
 export const GET = async (request: NextRequest) => addgmvCollection(request);
