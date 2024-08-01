026cc2094c30ec2813643c0a97c835b89355b424
Bounkhilatfati <fati.bounkhilat@gmail.com>
2024-08-01 12:12:57 +0100

    :recycle: refactor(remove file nextroute gmvByMonth):

--- src/app/api/orders/typesense/grossMarketValue/byMonth/route.ts
+++ src/app/api/orders/typesense/grossMarketValue/byMonth/route.ts
@@ -1,5 +1,48 @@
-import { NextRequest } from "next/server";
-import { nextRoute } from "@/clients/nextRoutes";
+import { responses } from "@/utils/responses";
+import { logError } from "@/utils/logError";
+import { NextResponse, type NextRequest } from "next/server";
+import { getGrossMarketValueByMonth } from "@/clients/typesense/orders/grossMarchandiseValue/getGrossMarchandiseValueByMonth";
+export const GET = async (request: NextRequest) => {
+  try {
+    const { searchParams } = new URL(request.url);
 
-export const GET = async (request: NextRequest) =>
-  nextRoute.orders.typesense.grossMarketValue.byMonth(request);
+    const yearString = searchParams.get("year");
+    const monthString = searchParams.get("month");
+
+    if (!yearString) {
+      return responses.invalidRequest("Year Parameter is Required");
+    }
+    if (!monthString) {
+      return responses.invalidRequest("Month Parameter is Required");
+    }
+    const year = parseInt(yearString, 10);
+    const month = parseInt(monthString, 10);
+
+    const gmv: number | undefined = await getGrossMarketValueByMonth(
+      year,
+      month,
+    );
+
+    if (!gmv) {
+      return responses.internalServerError(
+        "Gross Market Value By Month is Undefined",
+      );
+    }
+
+    const date = year + "-M" + month;
+
+    return NextResponse.json(
+      {
+        message: "success",
+        gmv,
+        date,
+      },
+      {
+        status: 200,
+      },
+    );
+  } catch (error: any) {
+    logError(error);
+    return responses.internalServerError();
+  }
+};
