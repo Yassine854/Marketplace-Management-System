2989cbaa20ff84635f7d5c53a602620fa7e5e233
Haythem532002 <khiarih06@gmail.com>
2024-08-01 15:14:25 +0100

    :recycle: refactor(refactor gmv by day):

--- src/app/api/analytics/gmv/byDay/route.ts
+++ src/app/api/analytics/gmv/byDay/route.ts
@@ -1,5 +1,30 @@
-import { NextRequest } from "next/server";
-import { nextRoute } from "@/clients/nextRoutes";
+import { responses } from "@/utils/responses";
+import { logError } from "@/utils/logError";
+import { NextResponse, type NextRequest } from "next/server";
+import { gmvByDayAnalytics } from "@/services/analytics/gmv/gmvByDayAnalytics";
 
-export const GET = async (request: NextRequest) =>
-  nextRoute.analytics.grossMarketValue.byDay(request);
+export const GET = async (request: NextRequest) => {
+  try {
+    const { searchParams } = new URL(request.url);
+
+    const isoDate = searchParams.get("date");
+
+    if (!isoDate || typeof isoDate !== "string") {
+      throw new Error("Date parameter is required");
+    }
+
+    const res = await gmvByDayAnalytics(isoDate);
+    return NextResponse.json(
+      {
+        message: "success",
+        data: res,
+      },
+      {
+        status: 200,
+      },
+    );
+  } catch (error: any) {
+    logError(error);
+    return responses.internalServerError();
+  }
+};
