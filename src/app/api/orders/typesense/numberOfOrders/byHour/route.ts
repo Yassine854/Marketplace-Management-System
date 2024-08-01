809bfb7e72cde91a68e0d62d086b67214928a612
Bounkhilatfati <fati.bounkhilat@gmail.com>
2024-08-01 11:38:51 +0100

    :recycle: refactor(remove file nextroutes noo/ByHour):

--- src/app/api/orders/typesense/numberOfOrders/byHour/route.ts
+++ src/app/api/orders/typesense/numberOfOrders/byHour/route.ts
@@ -1,5 +1,43 @@
-import { NextRequest } from "next/server";
+/*import { NextRequest } from "next/server";
 import { nextRoute } from "@/clients/nextRoutes";
 
 export const GET = async (request: NextRequest) =>
-  nextRoute.orders.typesense.numberOfOrders.byDay(request);
+  nextRoute.orders.typesense.numberOfOrders.byDay(request);*/
+
+import { responses } from "@/utils/responses";
+import { logError } from "@/utils/logError";
+import { NextResponse, type NextRequest } from "next/server";
+import { getNumberOfOrdersByDay } from "@/services/orders/typesense/numberOfOrders/getNumberOfOrdersByDay";
+
+export const GET = async (request: NextRequest) => {
+  try {
+    const { searchParams } = new URL(request.url);
+
+    const date = searchParams.get("date");
+
+    if (!date) {
+      return responses.invalidRequest("Date Parameter is Required");
+    }
+
+    const numberOfOrders: number | undefined = await getNumberOfOrdersByDay(
+      date,
+    );
+
+    if (!numberOfOrders) {
+      return responses.internalServerError("Number of Orders is Undefined");
+    }
+    return NextResponse.json(
+      {
+        message: "success",
+        numberOfOrders,
+        date,
+      },
+      {
+        status: 200,
+      },
+    );
+  } catch (error: any) {
+    logError(error);
+    return responses.internalServerError();
+  }
+};
