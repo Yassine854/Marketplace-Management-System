01f9da9987eb19563fdc166f7d09dbbbd659a063
Haythem532002 <khiarih06@gmail.com>
2024-08-01 15:35:45 +0100

    :recycle: refactor(number of unique customer by month):

--- src/app/api/analytics/numberOfUniqueCustomer/byMonth/route.ts
+++ src/app/api/analytics/numberOfUniqueCustomer/byMonth/route.ts
@@ -1,5 +1,29 @@
-import { NextRequest } from "next/server";
-import { nextRoute } from "@/clients/nextRoutes";
+import { responses } from "@/utils/responses";
+import { logError } from "@/utils/logError";
+import { NextResponse, type NextRequest } from "next/server";
+import { numberOfUniqueCustometerByMonthAnalytics } from "@/services/analytics/numberOfUniqueCustomer/byMonth";
 
-export const GET = async (request: NextRequest) =>
-  nextRoute.analytics.numberOfUniqueCustomer.byMonth(request);
+export const GET = async (request: NextRequest) => {
+  try {
+    const { searchParams } = new URL(request.url);
+
+    const date = searchParams.get("date");
+
+    if (!date) {
+      return responses.invalidRequest("Date Parameter is Required");
+    }
+    const res = await numberOfUniqueCustometerByMonthAnalytics(date);
+    return NextResponse.json(
+      {
+        message: "success",
+        data: res,
+      },
+      {
+        status: 200,
+      },
+    );
+  } catch (error: any) {
+    logError(error);
+    return responses.internalServerError();
+  }
+};
