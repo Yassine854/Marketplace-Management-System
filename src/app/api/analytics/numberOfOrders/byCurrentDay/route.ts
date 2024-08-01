d782f02e45f8381e63f974938771e4ecf4758769
Haythem532002 <khiarih06@gmail.com>
2024-08-01 15:34:27 +0100

    :recycle: refactor(number of orders by crrent dat):

--- src/app/api/analytics/numberOfOrders/byCurrentDay/route.ts
+++ src/app/api/analytics/numberOfOrders/byCurrentDay/route.ts
@@ -1,5 +1,29 @@
-import { NextRequest } from "next/server";
-import { nextRoute } from "@/clients/nextRoutes";
+import { responses } from "@/utils/responses";
+import { logError } from "@/utils/logError";
+import { NextResponse, type NextRequest } from "next/server";
+import { numberOfOrderByCurrentDayAnalytics } from "@/services/analytics/numberOfOrdersByCurrentDayAnalytics";
 
-export const GET = async (request: NextRequest) =>
-  nextRoute.analytics.numberOfOrders.byCurrentDay(request);
+export const GET = async (request: NextRequest) => {
+  try {
+    const { searchParams } = new URL(request.url);
+
+    const currentDay = searchParams.get("day");
+
+    if (!currentDay) {
+      return responses.invalidRequest("Date Parameter is Required");
+    }
+    const res = await numberOfOrderByCurrentDayAnalytics(currentDay);
+    return NextResponse.json(
+      {
+        message: "success",
+        data: res,
+      },
+      {
+        status: 200,
+      },
+    );
+  } catch (error: any) {
+    logError(error);
+    return responses.internalServerError();
+  }
+};
