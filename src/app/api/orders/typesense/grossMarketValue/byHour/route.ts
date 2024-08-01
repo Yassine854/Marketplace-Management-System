e66ebd74cf11b392fa462b5d10206977b4a9bc75
Bounkhilatfati <fati.bounkhilat@gmail.com>
2024-08-01 12:10:15 +0100

    :recycle: refactor(remove file nextroute gmvByHour):

--- src/app/api/orders/typesense/grossMarketValue/byHour/route.ts
+++ src/app/api/orders/typesense/grossMarketValue/byHour/route.ts
@@ -1,5 +1,43 @@
-import { NextRequest } from "next/server";
-import { nextRoute } from "@/clients/nextRoutes";
+import { getGrossMarketValueByHour } from "@/clients/typesense/orders/grossMarchandiseValue/getGrossMarchandiseValueByHour";
+import { responses } from "@/utils/responses";
+import { logError } from "@/utils/logError";
+import { NextResponse, type NextRequest } from "next/server";
+export const GET = async (request: NextRequest) => {
+  try {
+    const { searchParams } = new URL(request.url);
 
-export const GET = async (request: NextRequest) =>
-  nextRoute.orders.typesense.grossMarketValue.byHour(request);
+    const date = searchParams.get("date");
+
+    if (!date || typeof date !== "string") {
+      throw new Error("Date parameter is required");
+    }
+
+    const [year, month, day] = date.split("-").map(Number);
+
+    const gmv: [string, number][] | undefined = await getGrossMarketValueByHour(
+      year,
+      month,
+      day,
+    );
+
+    if (!gmv) {
+      return responses.internalServerError(
+        "Gross Market Value Day By Hour is Undefined",
+      );
+    }
+
+    return NextResponse.json(
+      {
+        message: "success",
+        gmv,
+        date,
+      },
+      {
+        status: 200,
+      },
+    );
+  } catch (error: any) {
+    logError(error);
+    return responses.internalServerError();
+  }
+};
