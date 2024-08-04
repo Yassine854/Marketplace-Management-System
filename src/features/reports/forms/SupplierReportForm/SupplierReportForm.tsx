9d18df6b0d93c44913e2fd8c6bb8f454f60a77c5
M.Jrad <mjrad@kamioun.tn>
2024-08-04 20:09:25 +0100

    fix date format

--- src/features/reports/forms/SupplierReportForm/SupplierReportForm.tsx
+++ src/features/reports/forms/SupplierReportForm/SupplierReportForm.tsx
@@ -15,7 +15,7 @@ function formatDate(inputDate: string) {
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const year = date.getFullYear();
 
-  return `${month}/${day}/${year}`;
+  return `${day}/${month}/${year}`;
 }
 
 const SuppliersReportForm = () => {
