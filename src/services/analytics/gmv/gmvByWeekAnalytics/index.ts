25af80bb59f32e65eddbabdb82a6111a159f136d
medazizktata <aziiizktata7@gmail.com>
2024-08-05 16:52:28 +0100

    gmvByWeek analytics endpoint hooked with gmvPreviousDays Collection

--- src/services/analytics/gmv/gmvByWeekAnalytics/index.ts
+++ src/services/analytics/gmv/gmvByWeekAnalytics/index.ts
@@ -1,18 +1,16 @@
 import dayjs from "dayjs";
 import weekOfYear from "dayjs/plugin/weekOfYear";
 import isoWeek from "dayjs/plugin/isoWeek";
-import { getGmvByDay } from "@/clients/typesense/orders/gmv/getGmvByDay";
 import { logError } from "@/utils/logError";
 import { typesenseClient } from "@/clients/typesense/typesenseClient";
 import { NextResponse } from "next/server";
-import { dateYMDToUnixTimestamp } from "@/utils/unixTimestamp";
 
 dayjs.extend(weekOfYear);
 dayjs.extend(isoWeek);
 
 type DataType = {
-  WeekDays: string[];
-  gmv: number[];
+  day: string;
+  gmv: number;
 };
 
 type GmvDocument = {
@@ -24,67 +22,56 @@ type GmvDocument = {
 };
 
 type SearchResult = {
-  hits: Array<{ document: GmvDocument }>;
+  hits?: Array<{ document: GmvDocument }>;
 };
 
 export const gmvByWeekAnalytics = async (yearArg: number, weekArg: number) => {
   try {
-    let WeekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+    const gmvData: DataType[] = [];
 
-    const startCurrentWeek = dayjs()
-      .year(yearArg)
-      .isoWeek(weekArg)
-      .startOf("isoWeek");
-    const startCurrentWeekFormatted = dateYMDToUnixTimestamp(
-      startCurrentWeek.format("YYYY-M-D"),
-    );
-    console.log("startCurrentWeek: ", startCurrentWeekFormatted);
-
-    const endCurrentWeek = startCurrentWeek.endOf("isoWeek");
-    const endCurrentWeekFormatted = dateYMDToUnixTimestamp(
-      endCurrentWeek.format("YYYY-M-D"),
-    );
-    console.log(
-      "🚀 ~ gmvByWeekAnalytics ~ endCurrentWeek:",
-      endCurrentWeekFormatted,
-    );
-
-    const gmv: number[] = [];
-    let allOrders: number[] = [];
     try {
       const searchParams = {
-        filter_by: `id:=[${startCurrentWeekFormatted}..${endCurrentWeekFormatted}]`,
+        filter_by: `year:=[${yearArg}] && week:=[${weekArg}]`,
         q: "*",
-        query_by: "year,month,day",
         page: 1,
         per_page: 250,
       };
 
-      const searchResults = await typesenseClient
+      const searchResults: SearchResult = await typesenseClient
         .collections("gmvPreviousDays")
         .documents()
         .search(searchParams);
 
-      console.log("🚀 ~ gmvByWeekAnalytics ~ searchResults:", searchResults);
-      const hits = searchResults.hits || [];
-      //@ts-ignore
-      allOrders = hits.map((hit) => hit.document.gmv);
-      console.log("🚀 ~ gmvByWeekAnalytics ~ allOrders:", allOrders);
+      const hits = searchResults.hits ?? [];
+
+      const allOrders = hits
+        .map((hit) => hit.document)
+        .sort((a, b) => {
+          return (
+            new Date(`${a.year}-${a.month}-${a.day}`).getTime() -
+            new Date(`${b.year}-${b.month}-${b.day}`).getTime()
+          );
+        });
+
+      allOrders.forEach((order) => {
+        const formattedDate = dayjs(
+          `${order.year}-${order.month}-${order.day}`,
+        ).format("ddd, DD-MM-YYYY"); // Format as "Mon", "Tue", etc.
+        gmvData.push({
+          day: formattedDate,
+          gmv: order.gmv,
+        });
+      });
     } catch (error: any) {
       logError(error);
     }
 
-    const data: DataType = {
-      WeekDays,
-      gmv: allOrders,
-    };
-
-    return data;
+    return gmvData;
   } catch (error: unknown) {
     if (error instanceof Error) {
-      logError(`Error fetching gmv per week data: ${error.message}`);
+      logError(`Error fetching GMV per week data: ${error.message}`);
     } else {
-      logError("Unknown error fetching gmv per week data");
+      logError("Unknown error fetching GMV per week data");
     }
     return NextResponse.json(
       { error: "Failed to fetch data" },
