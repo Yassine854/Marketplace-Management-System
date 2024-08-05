8a5dc9109e4356c1387183496f71fc5b019eb4b0
Haythem532002 <khiarih06@gmail.com>
2024-08-05 11:35:49 +0100

    :sparkles: feat(features/analytics/GrossMarchandiseValue): gmv by year chart

--- src/features/analytics/pages/Dashboard/Dashboard.tsx
+++ src/features/analytics/pages/Dashboard/Dashboard.tsx
@@ -2,6 +2,7 @@ import { useGetDeliveryAgents } from "@/features/shared/hooks/queries/useGetDeli
 import NumberOfOrdersChart from "../../widgets/NumberOfOrdersChart";
 import NumberOfUniqueCustomer from "../../widgets/NumberOfUniqueCustomer";
 import { useEffect } from "react";
+import GrossMarchandiseValue from "../../widgets/GrossMarchandiseValue";
 
 const Dashboard = () => {
   const { isLoading, deliveryAgents } = useGetDeliveryAgents();
@@ -16,9 +17,10 @@ const Dashboard = () => {
 
   return (
     <div className="mt-[4.8rem] w-full bg-n20 p-4">
-      <NumberOfOrdersChart />
-      <div className="mt-10">
+      <div className="flex h-[1500px] flex-col justify-between">
+        <NumberOfOrdersChart />
         <NumberOfUniqueCustomer />
+        <GrossMarchandiseValue />
       </div>
     </div>
   );
