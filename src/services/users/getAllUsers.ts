5ad968beab97c54b1dde2cc9d0fa5decae855562
NessrineDagh <daghbajinessrine@gmail.com>
2024-08-05 15:09:40 +0100

    :sparkles: feat(users/getAllUsers):

--- src/services/users/getAllUsers.ts
+++ src/services/users/getAllUsers.ts
@@ -1,20 +1,16 @@
 import { prisma } from "@/clients/prisma";
-import { UsersPayload } from "@/types/resolvers.types";
 
-export const getAllUsers = async (): Promise<UsersPayload> => {
+export const getAllUsers = async () => {
   try {
     const users = await prisma.getAllUsers();
 
     if (users && users?.length > 0) {
       return {
-        //@ts-ignore
-        users,
-        success: true,
         message: "Users fetched successfully",
+        users,
       };
     } else {
       return {
-        success: true,
         message: "No users found",
       };
     }
@@ -22,7 +18,6 @@ export const getAllUsers = async (): Promise<UsersPayload> => {
     process.env.NODE_ENV === "development" &&
       console.error("Error fetching users:", error);
     return {
-      success: false,
       message: error.message || "An error occurred while fetching users",
     };
   }
