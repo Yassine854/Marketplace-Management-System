ae4f290dc3bfb07b1cc1f02fed868623c947ef26
Haythem532002 <khiarih06@gmail.com>
2024-08-06 16:34:46 +0100

    :sparkles: feat:

--- next.config.js
+++ next.config.js
@@ -10,8 +10,6 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 
 const nextConfig = {
   reactStrictMode: false,
-  output: "standalone",
-  ssg: false,
 };
 
 module.exports = withNextIntl(withBundleAnalyzer(nextConfig));
