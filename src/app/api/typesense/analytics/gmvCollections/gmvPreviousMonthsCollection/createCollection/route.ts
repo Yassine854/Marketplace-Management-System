7c7902f393a66e35ce7cc7f1a5baf38b4d2bfa79
NessrineDagh <daghbajinessrine@gmail.com>
2024-08-14 15:06:08 +0100

    :sparkles: feat(gmvCollectionsLogic-PreviousMonths): gmvCollectionsLogic-PreviousMonths

---
+++ src/app/api/typesense/analytics/gmvCollections/gmvPreviousMonthsCollection/createCollection/route.ts
@@ -0,0 +1,28 @@
+import { createCollection } from "@/clients/typesense/createCollection";
+import { gmvPreviousMonthsCollectionSchema } from "@/clients/typesense/schemas/gmvPreviousMonthsCollection";
+import { NextRequest, NextResponse } from "next/server";
+import {
+  successResponse,
+  conflictResponse,
+  invalidRequestResponse,
+  internalServerErrorResponse,
+} from "src/utils/responses/typesenseResponses/CollectionHandlingResponse";
+
+export const GET = async (request: NextRequest) => {
+  const result: any = await createCollection(gmvPreviousMonthsCollectionSchema);
+
+  if (result.success) {
+    return successResponse(
+      gmvPreviousMonthsCollectionSchema.name + " created successfully.",
+    );
+  }
+
+  switch (result.message) {
+    case "conflict":
+      return conflictResponse();
+    case "internal_server_error":
+      return internalServerErrorResponse();
+    default:
+      return invalidRequestResponse(result.message);
+  }
+};
