4cbb015ab2ed11a10f98dbeb846a07a1000eee5a
medazizktata <aziiizktata7@gmail.com>
2024-08-19 16:45:37 +0100

    Seperation of Audit Logs (orders and milkRun)

---
+++ src/services/auditing/orders/index.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+import { AuditLog } from "@/types/AuditLog";
+
+const prisma = new PrismaClient();
+
+export async function createAuditLog(auditObject: AuditLog) {
+  const { username, userId, action, actionTime, orderId, storeId } =
+    auditObject;
+  //@ts-ignore
+  await prisma.ordersAuditTrail.create({
+    data: {
+      username,
+      userId,
+      action,
+      actionTime: new Date(),
+      orderId,
+      storeId,
+    },
+  });
+}
