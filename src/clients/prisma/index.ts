b228f6bd6edb6465f377dce37e277ec69e6fef97
Bounkhilatfati <fati.bounkhilat@gmail.com>
2024-08-23 13:03:17 +0100

    getMilkRunAuditLogs endpoint

--- src/clients/prisma/index.ts
+++ src/clients/prisma/index.ts
@@ -5,10 +5,12 @@ import { getUser } from "./getUser";
 import { editUser } from "./editUser";
 import { getAllUsers } from "./getAllUsers";
 import { getAllOrdersLogs } from "./getAllOrdersLogs";
+import { getAllMilkRunAuditLogs } from "./getAllMilkRunAuditLogs";
 
 export const prisma = {
   getUser,
   editUser,
   getAllUsers,
   getAllOrdersLogs,
+  getAllMilkRunAuditLogs,
 };
