0a35cbeaa1a9196f32eacb6d139c1487f0ead523
Your Name <you@example.com>
2025-04-08 16:58:59 +0200

    console

--- src/clients/prisma/getAllLog.ts
+++ src/clients/prisma/getAllLog.ts
@@ -2,9 +2,7 @@ import { prismaClient } from "./prismaClient";
 
 export const getAllLogs = async () => {
   try {
-    console.log("fetching logs from the database");
     const logs = await prismaClient.log.findMany();
-    console.log("logs fetched", logs);
 
     if (!logs || logs.length === 0) {
       throw new Error("No logs found");
@@ -12,7 +10,6 @@ export const getAllLogs = async () => {
 
     return logs;
   } catch (err) {
-    console.error("Get Prisma All Logs Error: ", err);
     return [];
   }
 };
